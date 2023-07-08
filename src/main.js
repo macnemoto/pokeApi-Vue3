c675d2634bd11c85a29b0c3b43fb7cab6f51837f
macnemoto <marcosj465@gmail.com>
2023-07-08 12:05:54 -0400

    :heavy_plus_sign: add a dependency pinia

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
-
+app.use(pinia)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/store/conuter.js'
+import { storeToRefs } from 'pinia'
+
+const useCounter = useCounterStore()
+const { increment } = useCounter
+const { double, count } = storeToRefs(useCounter)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+<h1 class="text-light"> Es numero es
+{{ count }}
+</h1>
+<button @click="increment">aumentar</button>
+<h2>Doble {{ double }}</h2>
+
   </main>
 </template>
