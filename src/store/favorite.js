d84e2431016c1f1206284efa71f10cc8165a4a44
macnemoto <marcosj465@gmail.com>
2023-07-09 23:03:45 -0400

    :sparkles: add more info in favorite

--- src/store/favorite.js
+++ src/store/favorite.js
@@ -5,15 +5,20 @@ import { ref } from 'vue'
 export const useFavoriteStore = defineStore('favorite', () => {
   const favorites = ref([])
 
+  if (localStorage.getItem('favoritos')) {
+    favorites.value = JSON.parse(localStorage.getItem('favoritos'))
+  }
+
   const add = (poke) => {
     favorites.value.push(poke)
+    localStorage.setItem('favoritos', JSON.stringify(favorites.value))
   }
   const remove = (id) => {
     favorites.value = favorites.value.filter(item => item.id !== id)
+    localStorage.setItem('favoritos', JSON.stringify(favorites.value))
   }
   const findPoke = (name) => {
     return favorites.value.find((item) => item.name === name)
-    // console.log('funciono2')
   }
 
   return {
--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useFavoriteStore } from '@/store/favorite.js'
 import { storeToRefs } from 'pinia'
+import { RouterLink } from 'vue-router'
 
 const useFavorites = useFavoriteStore()
 const { favorites } = storeToRefs(useFavorites)
@@ -20,6 +21,7 @@ Sin favoritos
                 {{ poke.name }}
             </div>
             <div>
+                <router-link class="text-decoration-none btn btn-sm btn-primary m-2" :to="`/pokemon/${poke.name}`">More Info: {{ poke.name }}</router-link>
                 <button class="btn btn-sm btn-danger" @click="remove(poke.id)" >Eliminar</button>
             </div>
         </li>
